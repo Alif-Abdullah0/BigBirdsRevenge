dc396de2f5136a105931a1972a5c863c21065990
cnelson20 <cjnelson2004@gmail.com>
2022-03-02 14:54:03 +0000

    login signup

--- app/__init__.py
+++ app/__init__.py
@@ -28,17 +28,34 @@ db.close()
 def index():
     return "Hello World";
        
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
+    """
+        Lets user log in
+    """
     if request.method == 'GET':
-        return "GET";
+        return """<!DOCTYPE html> <html> <body> <form action="/signup" method="POST"> Username: <input type='text' name='username'> <br> <br> Password: <input type='text' name='password'> <br> <br> <input type='submit' value='Submit'> </form></body> </html>
+        """;
     else:
         return "POST";
+        db = sqlite3.connect(MAIN_DB);
+        c = db.cursor();
+        c.execute("""SELECT hash FROM users WHERE USERNAME = ?""", (request.form.get('username'),));
+        correct = c.fetchone();
+        if correct == None:
+            return 'No such user exists!';
+        if correct != request.form.get('password'):
+            return 'Incorrect password!';
+        session['username'] = request.form.get('username');
+        return redirect('/');
     
 @app.route("/signup", methods=['GET','POST'])
 def signup():
+    """
+        Signs up user
+    """
     if request.method == 'GET':
-        return """<!DOCTYPE html> <html> <body> <form action="/signup" method="POST"> Username: <input type='text' name='username'> <br> <br> Password: <input type='text' name='password'> <input type='submit' value='Submit'> </form></body> </html>
+        return """<!DOCTYPE html> <html> <body> <form action="/signup" method="POST"> Username: <input type='text' name='username'> <br> <br> Password: <input type='text' name='password'> <br> <br> <input type='submit' value='Submit'> </form></body> </html>
         """;
     else:
         if not ('username' in request.form):
@@ -47,18 +64,26 @@ def signup():
             return 'Password required!';
         user = request.form.get('username');
         passw = request.form.get('password');
-        db = sqlite3.connect(MAIN_DB)
+        db = sqlite3.connect(MAIN_DB);
+        c = db.cursor();
         c.execute("""SELECT * FROM users WHERE USERNAME = ?""", (user,));
-        if c.fetchone != None:
+        if c.fetchone() != None:
             return 'Username already exists!';
         if user.isalnum() and not (' ' in passw or '\\' in passw):
-            c = db.cursor();
             c.execute("""INSERT INTO users (username,hash) VALUES (?,?)""", (user, passw,));
             db.commit();
             return redirect('/login');
         else:
             return 'No spaces or backslashes in usernames!';
         return "POST";
+
+@app.route("/logout")
+def logout():
+    """
+        Logouts user
+    """
+    session.pop('username', default=None)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True;
